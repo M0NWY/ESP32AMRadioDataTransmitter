5264b082911f508e22241fdadbf150d7039c1e03
Simon Newhouse <simon.newhouse@gmail.com>
2025-02-06 15:15:45 +0000

    Update AMTransmitterI2S.ino

--- AMTransmitterI2S/AMTransmitterI2S.ino
+++ AMTransmitterI2S/AMTransmitterI2S.ino
@@ -37,8 +37,13 @@ void setup()
 
 //buffer to store modulated samples, I2S samples of the esp32 are always 16Bit
 short buff[1024];
+
 //sine represented in 16 values. at 13MHz sampling rate the resulting carrier is at around 835KHz
 int sintab[] = {0, 48, 90, 117, 127, 117, 90, 48, 0, -48, -90, -117, -127, -117, -90, -48};
+
+// a sine represented as 65 values gives a frequency of 200khz - 66 values gives 196khz. grumble, the current signal is 198khz, either should work.
+// 65
+int sintab200[] = {0, 12, 25, 37, 48, 59, 70, 80, 89, 98, 105, 112, 117, 122, 125, 127, 128, 128, 126, 124, 120, 115, 109, 102, 94, 85, 75, 65, 54, 42, 31, 18, 6, -6, -18, -31, -42, -54, -65, -75, -85, -94, -102, -109, -115, -120, -124, -126, -128, -128, -127, -125, -122, -117, -112, -105, -98, -89, -80, -70, -59, -48, -37, -25, -12 } ;
 
 unsigned long long pos = 0;         //current position in the audio sample, using fixed point
 unsigned int posLow = 0;
